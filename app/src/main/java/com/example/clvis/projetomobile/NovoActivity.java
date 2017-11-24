80e2af5e06e14265de8df05423c9db58b57b7a81
Gabriel G. Aiala <ggaiala@hotmail.com>
2017-11-24 13:58:47 -0200

    aula 15 projeto

---
+++ app/src/main/java/com/example/clvis/projetomobile/NovoActivity.java
@@ -0,0 +1,13 @@
+package com.example.clvis.projetomobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NovoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo);
+    }
+}
